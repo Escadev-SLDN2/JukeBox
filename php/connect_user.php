fec850b35481ffacb9b70d283cb1b9f41303437c
Bastien BALOUP <35816948+Bastien-Baloup@users.noreply.github.com>
2020-01-30 10:33:01 +0100

    ajout et tests des fonctions de gestion de connexion des users

--- php/connect_user.php
+++ php/connect_user.php
@@ -1,3 +1,48 @@
 <?php
 	include "users.php";
+	session_start();
 
+//	verifie les credentials du user,
+//	si elles sont valides, met ses donnes dans $_SESSION et renvoi une string vide,
+//	sinon renvoi une string correspondant à l'erreur
+	function connectUser(string $mail, string $pass){
+		$id = -1;
+		$user = array();
+		try{
+			if(empty($mail)){
+				throw new Exception("Please enter your email.");
+			} 
+
+			if(empty($pass)){
+				throw new Exception("Please enter your password.");
+			}
+			$id = getUserIdFromMail($mail);
+			if (isPasswdValid($id, $pass)){
+				$user = getUserFromId($id);
+				$_SESSION['user'] = $user['nick'];
+				$_SESSION['loggedIn'] = True;
+				return "";
+			}else{
+				throw new Exception("Invalid Password");
+			}
+			
+		}catch(Exception $e){
+			$_SESSION['loggedIn'] = False;
+			return ($e->getMessage());
+		}
+	}
+
+
+//	verifie si les donnes necessaires sont presentes dans $_SESSION
+//	renvoi un booleen
+	function isUserConnected(){
+		if( (isset($_SESSION["user"]) && (isset($_SESSION["loggedIn"])) && $_SESSION["loggedIn"]) === true){
+			return True;
+		}
+		return False;
+	}
+
+//	deconnecte le user
+	function disconnectUser(){
+		$_SESSION['loggedIn'] = False;
+	}
--- php/tests/test_connect_users.php
+++ php/tests/test_connect_users.php
@@ -4,5 +4,15 @@
 	error_reporting(E_ALL);
 	include "../connect_user.php";
 
-
+	echo (isUserConnected() ? "true" : "false")."<br>";
+	echo (connectUser("test2localhost", "azerty2"))."<br>";
+	echo (isUserConnected() ? "true" : "false")."<br>";
+	disconnectUser();
+	echo (isUserConnected() ? "true" : "false")."<br>";
+	echo (connectUser("test2localhost", "azerty"))."<br>";
+	echo (isUserConnected() ? "true" : "false")."<br>";
+	echo (connectUser("testlocalhost", "azerty"))."<br>";
+	echo (isUserConnected() ? "true" : "false")."<br>";
+	echo (connectUser("", ""))."<br>";
+	echo (isUserConnected() ? "true" : "false")."<br>";
 ?>
