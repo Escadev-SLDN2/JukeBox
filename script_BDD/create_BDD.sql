93d7a8215e305cab0b92b2dda7fc4c7b42260333
combo baraka <combo.baraka01@gmail.com>
2020-02-10 14:31:45 +0100

    création de la BDD et les 4 tableaux

--- script_BDD/create_BDD.sql
+++ script_BDD/create_BDD.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE jukebox CHARACTER SET 'utf8';
 USE jukebox;
 
+
 CREATE USER 'jukebox' IDENTIFIED BY 'A9Z8E1R2';
 GRANT ALL privileges ON `jukebox`.* TO 'jukebox'@'%';
 FLUSH PRIVILEGES;
