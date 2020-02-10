b46faecce65d3c35446f67c36a108150ac53d49b
Bastien BALOUP <35816948+Bastien-Baloup@users.noreply.github.com>
2020-02-10 15:48:42 +0100

    ajout de config_sample et implementation de config.conf

--- php/bdd.php
+++ php/bdd.php
@@ -1,7 +1,7 @@
 <?php
-
+    require_once "config.conf";
 //	cree le pdo si il ne l'est pas deja et renvoi sa reference
-    function DBConnect($server='localhost', $name='jukebox', $user='jukebox', $passwd='A9Z8E1R2'){
+    function DBConnect($server=SERVER, $name=NAME, $user=USER, $passwd=PASSWD){
         try{
             $pdo = new PDO("mysql:host=" . $server . ";dbname=" . $name, $user, $passwd);
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
