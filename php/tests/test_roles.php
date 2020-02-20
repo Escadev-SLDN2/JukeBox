512b41ead065e61587d605aa050671e5952164ad
Désir Thomas <thomasdesir.pro@gmail.com>
2020-02-20 11:29:24 +0100

    fix code formating

--- php/tests/test_roles.php
+++ php/tests/test_roles.php
@@ -1,55 +1,85 @@
 <?php
-	ini_set('display_errors', '1');
-	ini_set('display_startup_errors', '1');
-	error_reporting(E_ALL);
-	require_once "../users.php";
-	
-	if(User::isMailUsed("test2localhost")){
-        $user=User::getFromMail("test2localhost");	
-	}else{
-		$user=User::add("blabla", "bla", "test2localhost", "azerty2");
-	}
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+require_once "../users.php";
+
+if (User::isMailUsed("test2localhost")) {
+    $user = User::getFromMail("test2localhost");
+} else {
+    $user = User::add("blabla", "bla", "test2localhost", "azerty2");
+}
 
 /*	Le but ici est de tester la gestion des roles
 
-	try{echo "is SU :".((User::isSUSet()) ? "true" : "false")."<br>\n";}catch(Exception $e){echo($e->getMessage())."<br>\n";}
-		Permet d'afficher si un SU est desingne dans la bdd ou non .
+    try{echo "is SU :".((User::isSUSet()) ? "true" : "false")."<br>\n";}catch(Exception $e){echo($e->getMessage())."<br>\n";}
+        Permet d'afficher si un SU est desingne dans la bdd ou non .
 
-	echo "is connected :".(User::isConnected() ? "true" : "false")."<br>\n";
-		Permet d'afficher si un user est connecte dans cette session ou non .
+    echo "is connected :".(User::isConnected() ? "true" : "false")."<br>\n";
+        Permet d'afficher si un user est connecte dans cette session ou non .
 
-	try{(User::connect("test2localhost", "azerty2")); echo"<br>\n";} catch(Exception $e){echo($e->getMessage())."<br>\n";}
-		Connecte un user.
-	
-	try{$_SESSION['user']->changeRole(""); echo "action :set role SU<br>\n";}catch(Exception $e){echo($e->getMessage())."<br>\n";}
-		Change le role du user connecte actuellement.
+    try{(User::connect("test2localhost", "azerty2")); echo"<br>\n";} catch(Exception $e){echo($e->getMessage())."<br>\n";}
+        Connecte un user.
+    
+    try{$_SESSION['user']->changeRole(""); echo "action :set role SU<br>\n";}catch(Exception $e){echo($e->getMessage())."<br>\n";}
+        Change le role du user connecte actuellement.
 
 */
 
-	try{echo "is SU :".((User::isSUSet()) ? "true" : "false")."<br>\n";}catch(Exception $e){echo($e->getMessage())."<br>\n";}
-	echo "is connected :".(User::isConnected() ? "true" : "false")."<br>\n";
-	echo "action :connection ";
-	try{(User::connect("test2localhost", "azerty2")); echo"<br>\n";} catch(Exception $e){echo($e->getMessage())."<br>\n";}
-	echo "is connected :".(User::isConnected() ? "true" : "false")."<br>\n";
-	try{echo "is SU :".((User::isSUSet()) ? "true" : "false")."<br>\n";}catch(Exception $e){echo($e->getMessage())."<br>\n";}
-	try{$_SESSION['user']->changeRole("SU"); echo "action :set role SU<br>\n";}catch(Exception $e){echo($e->getMessage())."<br>\n";}
-	try{echo "is SU :".((User::isSUSet()) ? "true" : "false")."<br>\n";}catch(Exception $e){echo($e->getMessage())."<br>\n";}
-	try{$_SESSION['user']->changeRole("NULL"); echo "action :set role NULL<br>\n";}catch(Exception $e){echo($e->getMessage())."<br>\n";}
-	try{echo "is SU :".((User::isSUSet()) ? "true" : "false")."<br>\n";}catch(Exception $e){echo($e->getMessage())."<br>\n";}
-	User::disconnect();
-	$user->delete();
+try {
+    echo "is SU :" . ((User::isSUSet()) ? "true" : "false") . "<br>\n";
+} catch (Exception $e) {
+    echo ($e->getMessage()) . "<br>\n";
+}
+echo "is connected :" . (User::isConnected() ? "true" : "false") . "<br>\n";
+echo "action :connection ";
+try {
+    (User::connect("test2localhost", "azerty2"));
+    echo "<br>\n";
+} catch (Exception $e) {
+    echo ($e->getMessage()) . "<br>\n";
+}
+echo "is connected :" . (User::isConnected() ? "true" : "false") . "<br>\n";
+try {
+    echo "is SU :" . ((User::isSUSet()) ? "true" : "false") . "<br>\n";
+} catch (Exception $e) {
+    echo ($e->getMessage()) . "<br>\n";
+}
+try {
+    $_SESSION['user']->changeRole("SU");
+    echo "action :set role SU<br>\n";
+} catch (Exception $e) {
+    echo ($e->getMessage()) . "<br>\n";
+}
+try {
+    echo "is SU :" . ((User::isSUSet()) ? "true" : "false") . "<br>\n";
+} catch (Exception $e) {
+    echo ($e->getMessage()) . "<br>\n";
+}
+try {
+    $_SESSION['user']->changeRole("NULL");
+    echo "action :set role NULL<br>\n";
+} catch (Exception $e) {
+    echo ($e->getMessage()) . "<br>\n";
+}
+try {
+    echo "is SU :" . ((User::isSUSet()) ? "true" : "false") . "<br>\n";
+} catch (Exception $e) {
+    echo ($e->getMessage()) . "<br>\n";
+}
+User::disconnect();
+$user->delete();
 
 /* 
-	la page devrait afficher :
+    la page devrait afficher :
 
-		is SU :false
-		is connected :false
-		action :connection
-		is connected :true
-		is SU :false
-		action :set role SU
-		is SU :true
-		action :set role NULL
-		is SU :false
+        is SU :false
+        is connected :false
+        action :connection
+        is connected :true
+        is SU :false
+        action :set role SU
+        is SU :true
+        action :set role NULL
+        is SU :false
 */
-?>
--- php/videos.php
+++ php/videos.php
@@ -1,99 +1,103 @@
-<?php  
-    // Affichage des erreurs détaillées :
-    ini_set('display_errors', '1');
-    ini_set('display_startup_errors', '1');
-    error_reporting(E_ALL);
+<?php
+// Affichage des erreurs détaillées :
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
 
-    // Récupération des données 
-    require_once "bdd.php";
-    
-    //ajout d'une video à la BDD
-    function addvideos(string $id_yt, int $user_id){
-        $pdo =DBConnect();
-        $sql = "INSERT INTO videos (id_yt, user_id) VALUES (:id_yt, :user_id)";
+// Récupération des données 
+require_once "bdd.php";
 
-        //interdit les doublond grace à l'id_yt
-        if(GetIdVideo($id_yt) != -1){
-            throw new Exception("la video existe déja");
-        } else{
-            $stmt = $pdo -> prepare ($sql);
-            $stmt ->bindParam (':id_yt', $id_yt, PDO ::PARAM_STR);
-            $stmt ->bindParam (':user_id', $user_id, PDO ::PARAM_INT);
-            $stmt->execute();
-        }
+//ajout d'une video à la BDD
+function addvideos(string $id_yt, int $user_id)
+{
+    $pdo = DBConnect();
+    $sql = "INSERT INTO videos (id_yt, user_id) VALUES (:id_yt, :user_id)";
 
+    //interdit les doublond grace à l'id_yt
+    if (GetIdVideo($id_yt) != -1) {
+        throw new Exception("la video existe déja");
+    } else {
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':id_yt', $id_yt, PDO::PARAM_STR);
+        $stmt->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+        $stmt->execute();
     }
+}
 
-    /*
+/*
     recupérartion de l'id video garce à l'id_yt
     */
-    function GetIdVideo( string $id_yt ){
-        $pdo =DBConnect();
-        $sql = "SELECT id FROM videos WHERE id_yt='$id_yt'";
-        $result = $pdo->query($sql);
-        if($result->rowcount()>1){          //verrifie les doublons
-            throw new Exception('deux fois meme videos ');
-        } elseif($result->rowcount()==1){   //verifie qu'une seule video avec id_yt
-            $tab=$result->fetch();          //recupére une ligne depuis le resultat associé à la PDO
-            return $tab['id'];              //affiche id sous forme d'un tableau
-        }elseif($result->rowcount()==0){    //aucune video
-            return -1;
-        }
-
+function GetIdVideo(string $id_yt)
+{
+    $pdo = DBConnect();
+    $sql = "SELECT id FROM videos WHERE id_yt='$id_yt'";
+    $result = $pdo->query($sql);
+    if ($result->rowcount() > 1) {          //verrifie les doublons
+        throw new Exception('deux fois meme videos ');
+    } elseif ($result->rowcount() == 1) {   //verifie qu'une seule video avec id_yt
+        $tab = $result->fetch();          //recupére une ligne depuis le resultat associé à la PDO
+        return $tab['id'];              //affiche id sous forme d'un tableau
+    } elseif ($result->rowcount() == 0) {    //aucune video
+        return -1;
     }
-    
-    //modifier la video dans la BDD
-    function modifvideos(int $id,string $id_yt, int $user_id=-1){
-        $pdo = DBConnect();
-        $sql = "UPDATE videos SET ";
-        $nb = 0;
-        
-        if(strlen($id_yt) != 0){
+}
 
-            $sql .="id_yt = :id_yt";
-            $nb = $nb+1;
-        }
-        if( $user_id != -1){
-            if($nb >= 1){ 
-                $sql .=", ";
-            }
-                
-            $sql .="user_id= :user_id";
-            $nb = $nb+1;
-        }
-        
-        $sql .=" WHERE id=:id";
-        
+//modifier la video dans la BDD
+function modifvideos(int $id, string $id_yt, int $user_id = -1)
+{
+    $pdo = DBConnect();
+    $sql = "UPDATE videos SET ";
+    $nb = 0;
 
-        if($stmt = $pdo->prepare($sql)){
+    if (strlen($id_yt) != 0) {
+
+        $sql .= "id_yt = :id_yt";
+        $nb = $nb + 1;
+    }
+    if ($user_id != -1) {
+        if ($nb >= 1) {
+            $sql .= ", ";
+        }
+
+        $sql .= "user_id= :user_id";
+        $nb = $nb + 1;
+    }
+
+    $sql .= " WHERE id=:id";
+
+
+    if ($stmt = $pdo->prepare($sql)) {
         // lie les paramettres de la requette avec les variables correspondantes si elles ont ete passe en paramettres
-            $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
 
-            if(strlen($id_yt) != 0){$stmt->bindParam(":id_yt", $id_yt, PDO::PARAM_STR);}
-            if($user_id != -1){$stmt->bindParam(":user_id", $user_id, PDO::PARAM_INT);}
-            
-            if(!($stmt->execute())){
-                throw new Exception('Stmt->execute() error');
-            }
-        }else{
-            throw new Exception('$pdo->prepare($sql) error'); 
+        if (strlen($id_yt) != 0) {
+            $stmt->bindParam(":id_yt", $id_yt, PDO::PARAM_STR);
+        }
+        if ($user_id != -1) {
+            $stmt->bindParam(":user_id", $user_id, PDO::PARAM_INT);
+        }
+
+        if (!($stmt->execute())) {
+            throw new Exception('Stmt->execute() error');
+        }
+    } else {
+        throw new Exception('$pdo->prepare($sql) error');
+    }
+}
+
+// suppression d'une video dans la bdd
+function delvideos(int $id)
+{
+
+    $pdo = DBConnect();
+    $sql = "DELETE FROM videos WHERE id = :id";
+
+    if ($stmt = $pdo->prepare($sql)) {
+
+        $stmt->bindParam(":id", $id, PDO::PARAM_INT);
+
+        if (!($stmt->execute())) {
+            throw new Exception("Something went wrong. Please try again later.");
         }
     }
-    
-    // suppression d'une video dans la bdd
-    function delvideos(int $id){
-
-        $pdo =DBConnect();
-        $sql = "DELETE FROM videos WHERE id = :id";
-
-        if($stmt = $pdo->prepare($sql)){
-
-            $stmt->bindParam(":id", $id, PDO::PARAM_INT);
-
-            if(!($stmt->execute())){
-				throw new Exception("Something went wrong. Please try again later.");
-			}
-        }
-    }
-
-    
+}
