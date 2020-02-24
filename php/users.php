a0692228b7589789727a6b0ee6de88e2d0f97ab0
Bastien BALOUP <35816948+Bastien-Baloup@users.noreply.github.com>
2020-02-24 15:35:16 +0100

    correction de getSUId

--- php/users.php
+++ php/users.php
@@ -199,12 +199,12 @@ class User
         $pdo = DBConnect();
         $sql = "SELECT id FROM users WHERE role = 'ROLE_ADMIN'";
         $answer = $pdo->query($sql);
-        if ($answer->rowCount() == 1) {
+        } if ($answer->rowCount() == 0) {
+            return -1;
+        }else {
             $row = $answer->fetch();
             return $row['id'];
-        } else if ($answer->rowCount() == 0) {
-            return -1;
-        }
+        
     }
 
     //	verifie si un user a le role SU
