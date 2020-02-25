fcb55f5b8c35c008ef950c0197ac0ade0483b887
Papibadiste <labussieremailpro@gmail.com>
2020-02-25 11:21:14 +0100

    fix boutton

--- index.php
+++ index.php
@@ -105,7 +105,7 @@ if (!empty($msg)) {
 
                     <div class="row mb-1">
                         <?php if (!$isConn) { ?>
-                            <div class="col-5">
+                            <div class="col-3 offset-5">
                                 <button type="button" class="btn btn-danger ml-5 pl-3 my-3 boubou " data-toggle="modal" data-target="#inscriptionModal">
                                     Inscription
                                 </button>
@@ -171,7 +171,7 @@ if (!empty($msg)) {
 
                             <!--pop up 2-->
 
-                            <div class="col-5">
+                            <div class="col-3">
 
                                 <button type="button" class="btn btn-danger ml-5 pl-3 my-3 boubou <?php if ($isConn) {
                                                                                                     echo "d-none";
